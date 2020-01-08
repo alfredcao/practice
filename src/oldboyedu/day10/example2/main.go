f6db60e7b9dab91a090139a283d311b1b59b19ec
caozhen <caozhen@lattebank.com>
2020-01-08 09:20:33 +0800

    oldboyedu -> day10 -> 自定义HTTP客户端，自定义超时时间

--- src/oldboyedu/day10/example2/main.go
+++ src/oldboyedu/day10/example2/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
+	"time"
 )
 
 var urls = []string{
@@ -13,7 +15,16 @@ var urls = []string{
 
 func main() {
 	for _, url := range urls {
-		res, err := http.Head(url)
+		c := http.Client{
+			Transport: &http.Transport{
+				Dial: func(network, addr string) (net.Conn, error) {
+					return net.DialTimeout(network, addr, time.Second*2)
+				},
+			},
+		}
+
+		//res, err := http.Head(url)
+		res, err := c.Head(url)
 		if err != nil {
 			fmt.Printf("访问网站[%s]失败！err : %v\n", url, err)
 		} else {
