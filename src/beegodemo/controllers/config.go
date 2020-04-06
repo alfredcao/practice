8f4800d40850c7061e3ebe330b7f96123e3aaf3e
caozhen <caozhen@lattebank.com>
2020-04-06 16:57:05 +0800

    beego -> 控制器介绍

--- src/beegodemo/controllers/config.go
+++ src/beegodemo/controllers/config.go
@@ -3,25 +3,43 @@ package controllers
 import (
 	"fmt"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context"
 )
 
 type CustomConfigController struct {
 	beego.Controller
 }
 
-func (p *CustomConfigController) Get() {
+// 每次请求都会执行
+func (p *CustomConfigController) Init(ctx *context.Context, controllerName, actionName string, app interface{}) {
+	p.Controller.Init(ctx, controllerName, actionName, app)
+	fmt.Println("invoke Init()")
+}
+
+// 每次请求都会执行
+func (p *CustomConfigController) Prepare() {
+	fmt.Println("invoke Prepare()")
 	configKey := p.GetString("configKey")
 	if configKey == "" {
-		p.Ctx.WriteString("please pass valid configKey")
-		return
+		p.Data["json"] = map[string]interface{}{"success": false, "msg": "please pass valid configKey"}
+		p.ServeJSON()
+		p.StopRun()
 	}
+}
+
+// 每次请求都会执行
+func (p *CustomConfigController) Finish() {
+	fmt.Println("invoke Finish()")
+}
+
+func (p *CustomConfigController) Get() {
+	configKey := p.GetString("configKey")
 	v, err := beego.GetConfig("String", configKey, "")
 	if err != nil {
 		p.Ctx.WriteString(fmt.Sprintf("get config value failed, err : %v", err))
 		return
 	}
 	p.Ctx.WriteString(v.(string))
-
 }
 
 type AppConfigController struct {
