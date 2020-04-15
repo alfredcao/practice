c8e29048cfdbbe10bb2863e3454418d06838585f
caozhen <caozhen@lattebank.com>
2020-04-15 22:52:13 +0800

    类型断言，type-switch

--- src/spec/typeassertions/main.go
+++ src/spec/typeassertions/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 type I interface{ m() }
@@ -10,4 +11,17 @@ func main() {
 	var x interface{} = 7
 	v1, ok := x.(I)
 	fmt.Println(ok, v1)
+
+	var d = time.Second
+	x = d
+	v2, ok := x.(time.Duration)
+	fmt.Println(ok, v2)
+
+	switch t := x.(type) {
+	case int:
+		fmt.Println("int")
+	case time.Duration:
+		fmt.Println("time.Duration", t)
+	}
+
 }
